ff12832ad68bb909365b6c0fcdf0a0f20d0ee1d7
AdrianMartinezCode <amartinezm1994@gmail.com>
2022-05-19 08:43:01 +0200

    materializing streams lesson

--- udemy-akka-streams-own/src/main/scala/part2_primer/MaterializingStreams.scala
+++ udemy-akka-streams-own/src/main/scala/part2_primer/MaterializingStreams.scala
@@ -39,6 +39,17 @@ A component can materialize multiple times
 
 A materialized value can be ANYTHING!
 
+Recap
+
+Materializing a graph = materializing all components
+- each component produces a materialized value when run
+- the graph produces a single materialized value
+- our job is to choose which one to pick
+
+A component can materialize multiple times
+
+A materialized value can be ANYTHING
+
  */
 object MaterializingStreams extends App {
 
@@ -47,26 +58,26 @@ object MaterializingStreams extends App {
   import system.dispatcher
 
   val simpleGraph = Source(1 to 10).to(Sink.foreach(println))
-//  val simpleMaterializedValue = simpleGraph.run()
+  val simpleMaterializedValue = simpleGraph.run()
 
   val source = Source(1 to 10)
   val sink = Sink.reduce[Int]((a, b) => a + b)
   val sumFuture = source.runWith(sink)
-//  sumFuture.onComplete {
-//    case Success(value) => println(s"The sum of all elements is: $value")
-//    case Failure(ex) => println(s"The sum of the elements could not be computed: $ex")
-//  }
+  sumFuture.onComplete {
+    case Success(value) => println(s"The sum of all elements is: $value")
+    case Failure(ex) => println(s"The sum of the elements could not be computed: $ex")
+  }
 
   // choosing materialized values
   val simpleSource = Source(1 to 10)
   val simpleFlow = Flow[Int].map(x => x + 1)
   val simpleSink = Sink.foreach[Int](println)
-//  simpleSource.viaMat(simpleFlow)((sourceMat, flowMat) => flowMat) // or we can use Keep.right
+  simpleSource.viaMat(simpleFlow)((sourceMat, flowMat) => flowMat) // or we can use Keep.right
   val graph = simpleSource.viaMat(simpleFlow)(Keep.right).toMat(simpleSink)(Keep.right)
-//  graph.run().onComplete {
-//    case Success(_) => println("Stream processing finished")
-//    case Failure(ex) => println(s"Stream processing failed with: $ex")
-//  }
+  graph.run().onComplete {
+    case Success(_) => println("Stream processing finished")
+    case Failure(ex) => println(s"Stream processing failed with: $ex")
+  }
 
   // sugars
   val sum = Source(1 to 10).runWith(Sink.reduce(_ + _)) // source.to(Sink.reduce)(Keep.right) -> by default
@@ -78,9 +89,11 @@ object MaterializingStreams extends App {
   Source(1 to 10).runReduce(_ + _) // exactly the same
 
   // backwards
-  Sink.foreach[Int](println).runWith(Source.single(42)) // source(..).to(sink...).run()
+  Sink.foreach[Int](println)
+    .runWith(Source.single(42)) // source(..).to(sink...).run()
   // both ways
-  Flow[Int].map(x => 2 * x).runWith(simpleSource, simpleSink)
+  Flow[Int].map(x => 2 * x)
+    .runWith(simpleSource, simpleSink)
 
   /**
    * - return the last element out of a source (use Sink.last)
@@ -88,12 +101,57 @@ object MaterializingStreams extends App {
    *    - map, fold, reduce
    */
   val sourceElems = Source(1 to 10000)
-  val flowConvertToString = Flow[Int].map(Random.nextString(Random.nextInt(20)))
+
+  source
+    .runWith(Sink.last)
+    .onComplete{
+      case Success(elem) => println(s"Elem getted $elem")
+    }
+
+
+  val flowConvertToString = Flow[Int].map(_ => Random.nextString(Random.nextInt(50)))
+
+  val sourceWithFlowString = sourceElems.viaMat(flowConvertToString)(Keep.right)
 
   val flowFold = Flow[String].fold(0)(_ + _.length)
+  val flowReduce = Flow[String]
+    .map(_.length)
+    .reduce(_ + _)
 
-  sourceElems
-    .viaMat(flowConvertToString)(Keep.right)
+  val sinkExercise = Sink.foreach[Int](elem => println(s"Completed sink $elem"))
+
+  sourceWithFlowString
     .viaMat(flowFold)(Keep.right)
-    .runWith()
+    .runWith(sinkExercise)
+
+  sourceWithFlowString
+    .viaMat(flowReduce)(Keep.right)
+    .runWith(sinkExercise)
+
+
+  /*
+  Other solutions
+   */
+  val f1 = Source(1 to 10).toMat(Sink.last)(Keep.right).run()
+  val f2 = Source(1 to 10).runWith(Sink.last)
+
+  val sentenceSource = Source(List(
+    "Akka is awesome",
+    "I love streams",
+    "Materialized values are killing me"
+  ))
+  val f = (currentWords: Int, newSentence: String) => currentWords + newSentence.split(" ").length
+
+  val wordCountSink = Sink.fold[Int, String](0)(f)
+  val g1 = sentenceSource.toMat(wordCountSink)(Keep.right).run()
+  val g2 = sentenceSource.runWith(wordCountSink)
+  val g3 = sentenceSource.runFold(0)(f)
+
+  val wordCountFlow = Flow[String].fold[Int](0)(f)
+  val g4 = sentenceSource.via(wordCountFlow).toMat(Sink.head)(Keep.right).run()
+  val g5 = sentenceSource.viaMat(wordCountFlow)(Keep.left).toMat(Sink.head)(Keep.right).run()
+  val g6 = sentenceSource.via(wordCountFlow).runWith(Sink.head)
+  val g7 = wordCountFlow.runWith(sentenceSource, Sink.head)._2
+
+
 }
